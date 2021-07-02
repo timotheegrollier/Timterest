0adb7b7c4d1445c65495b7309a0565f989941d92
timotheegrollier <timotheegrollier@gmail.com>
2021-07-02 14:50:55 +0200

    Add flash messages

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -43,6 +43,8 @@ class PinsController extends AbstractController
           $em->persist($pin);
           $em->flush();
 
+          $this->addFlash('success','Pin successfully created !');
+
 
           return $this->redirectToRoute('app_home');
       }
@@ -84,6 +86,8 @@ class PinsController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()){
 
             $em->flush();
+          $this->addFlash('success','Pin successfully updated !');
+
             return $this->redirectToRoute('app_home');
         }
 
@@ -105,6 +109,8 @@ class PinsController extends AbstractController
          if($this->isCsrfTokenValid('pin_deletion_' . $pin->getId(),$request->request->get('csrf_token') )){
              $em->remove($pin);
              $em->flush();
+
+          $this->addFlash('info','Pin successfully deleted !');
 
          }
 
