35cb40e1a8c1a7ac0d54725ef9e648b5d81811b6
timotheegrollier <timotheegrollier@gmail.com>
2021-07-04 00:44:05 +0200

    Add validator Constraint for Pin entity

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Entity\Traits\Timestampable;
 use App\Repository\PinRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=PinRepository::class)
@@ -23,11 +24,17 @@ class Pin
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Title can't be blank")
+     * @Assert\Length(min=3)
+     * 
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank
+     * @Assert\Length(min=10)
+     * 
      */
     private $description;
 
