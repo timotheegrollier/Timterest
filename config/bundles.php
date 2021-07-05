c9d48e5926424a7e94817b302c895ff11105c5fd
timotheegrollier <timotheegrollier@gmail.com>
2021-07-05 13:28:10 +0200

    Make use of Webkpack encore and add lang attributes to html

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Fidry\PsyshBundle\PsyshBundle::class => ['all' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
