20e754d843f9feaa212223709db79c63ba6a8219
timotheegrollier <timotheegrollier@gmail.com>
2021-07-08 16:01:36 +0200

    Display pin owner and getFullName method for user's full name generation

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -207,4 +207,10 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function getFullName(): ?string
+    {
+        return $this->getFirstName() . ' ' . $this->getLastName();
+    }
+
 }
