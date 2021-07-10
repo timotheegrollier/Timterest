a700a97c4cb93dd5991f430dae6663338666b6b6
timotheegrollier <timotheegrollier@gmail.com>
2021-07-10 11:00:15 +0200

    Add authentication system and repair remember me with change on the session cookie

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -22,89 +22,102 @@ class PinsController extends AbstractController
      * @Route("/", name="app_home" ,methods="GET")
      */
     public function index(PinRepository $pinRepository): Response
-    {   
+    {
 
-        $pins = $pinRepository->findBy([],['createdAt' =>'DESC']);
+        // FOR REMEMBER ME FEATURE
+        
+        if (isset($_COOKIE['rem'])) {
+            if ($_COOKIE['rem'] == 1) {
+                session_start();
+                session_abort();
+                session_set_cookie_params(3600);
+                // dd('fuck');
+                unset($_COOKIE['rem']);
+                setcookie('rem', '', time() - 3600, '/');
+            }
+        }
+
+        // HOME DISPLAY
+        
+        $pins = $pinRepository->findBy([], ['createdAt' => 'DESC']);
         return $this->render('pins/index.html.twig', compact("pins"));
     }
 
 
- /**
+    /**
      * @Route("/pins/create", name="app_pins_create",methods={"GET","POST"})
      */
-    public function create(Request $request,EntityManagerInterface $em, UserRepository $userRepository):Response
+    public function create(Request $request, EntityManagerInterface $em, UserRepository $userRepository): Response
     {
 
         $pin = new Pin;
-      $form = $this->createForm(PinType::class,$pin)
-      ;
-
-      $form->handleRequest($request);
-
-
-      if ($form->isSubmitted() && $form->isValid()){
-          $johnDoe = $userRepository->findOneBy(['email' => 'john@example.com']);
-          $pin->setUser($johnDoe);
-          $em->persist($pin);
-          $em->flush();
-          $this->addFlash('success','Pin successfully created !');
-
-
-          return $this->redirectToRoute('app_home');
-      }
-        return $this->render('pins/create.html.twig', ['form'=>$form->createView()]);
-    }
-
-
-
-
-
-     /**
-     * @Route("/pins/{id<[0-9]+>}", name="app_pins_show" ,methods="GET")
-     */
-
-    public function show(Pin $pin):Response
-    {
-        return $this->render('pins/show.html.twig',compact('pin'));
-    }
-
-
-    
-
-
- /**
-     * @Route("/pins/{id<[0-9]+>}/edit", name="app_pins_edit" ,methods={"GET","PUT"})
-     */
-    public function edit(Request $request,Pin $pin,EntityManagerInterface $em):Response
-    {
-
-        $form = $this->createFormBuilder($pin,["method"=>"PUT"])
-        ->add('imageFile', VichImageType::class, [
-            'label' => 'Image (JPG or PNG file)',
-            'required' => false,
-            'allow_delete' => true,
-            'download_uri' => false,
-            'image_uri' => true,
-            'imagine_pattern'=>'squared_thumbnail_small'
-        ])
-        ->add('title',TextType::class)
-        ->add('description',TextareaType::class)
-        ->getForm()
-        ;
+        $form = $this->createForm(PinType::class, $pin);
 
         $form->handleRequest($request);
 
 
-        if ($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
+            $johnDoe = $userRepository->findOneBy(['email' => 'john@example.com']);
+            $pin->setUser($johnDoe);
+            $em->persist($pin);
+            $em->flush();
+            $this->addFlash('success', 'Pin successfully created !');
+
+
+            return $this->redirectToRoute('app_home');
+        }
+        return $this->render('pins/create.html.twig', ['form' => $form->createView()]);
+    }
+
+
+
+
+
+    /**
+     * @Route("/pins/{id<[0-9]+>}", name="app_pins_show" ,methods="GET")
+     */
+
+    public function show(Pin $pin): Response
+    {
+        return $this->render('pins/show.html.twig', compact('pin'));
+    }
+
+
+
+
+
+    /**
+     * @Route("/pins/{id<[0-9]+>}/edit", name="app_pins_edit" ,methods={"GET","PUT"})
+     */
+    public function edit(Request $request, Pin $pin, EntityManagerInterface $em): Response
+    {
+
+        $form = $this->createFormBuilder($pin, ["method" => "PUT"])
+            ->add('imageFile', VichImageType::class, [
+                'label' => 'Image (JPG or PNG file)',
+                'required' => false,
+                'allow_delete' => true,
+                'download_uri' => false,
+                'image_uri' => true,
+                'imagine_pattern' => 'squared_thumbnail_small'
+            ])
+            ->add('title', TextType::class)
+            ->add('description', TextareaType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
 
             $em->flush();
-          $this->addFlash('success','Pin successfully updated !');
+            $this->addFlash('success', 'Pin successfully updated !');
 
             return $this->redirectToRoute('app_home');
         }
 
-        
-        return $this->render('pins/edit.html.twig', ['pin'=>$pin,'form'=>$form->createView()]);
+
+        return $this->render('pins/edit.html.twig', ['pin' => $pin, 'form' => $form->createView()]);
     }
 
 
@@ -114,18 +127,17 @@ class PinsController extends AbstractController
      */
 
 
-     public function delete(Request $request, Pin $pin , EntityManagerInterface $em):Response
-     {
+    public function delete(Request $request, Pin $pin, EntityManagerInterface $em): Response
+    {
 
         // dd($request->request->all());
-         if($this->isCsrfTokenValid('pin_deletion_' . $pin->getId(),$request->request->get('csrf_token') )){
-             $em->remove($pin);
-             $em->flush();
+        if ($this->isCsrfTokenValid('pin_deletion_' . $pin->getId(), $request->request->get('csrf_token'))) {
+            $em->remove($pin);
+            $em->flush();
 
-          $this->addFlash('info','Pin successfully deleted !');
-
-         }
+            $this->addFlash('info', 'Pin successfully deleted !');
+        }
 
         return $this->redirectToRoute('app_home');
-     }
+    }
 }
