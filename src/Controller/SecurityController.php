cec0811d2e59822f2c23053b5e61f649eaa6ad1d
timotheegrollier <timotheegrollier@gmail.com>
2021-07-21 13:50:35 +0200

    use POST for logout and add CSRF token

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -16,7 +16,7 @@ class SecurityController extends AbstractController
     {
         if ($this->getUser()) {
 
-            $this->addFlash("error","Déja connecté !");
+            $this->addFlash("error", "Déja connecté !");
             return $this->redirectToRoute('app_home');
         }
 
@@ -25,13 +25,13 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-       
+
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
     /**
-     * @Route("/logout", name="app_logout")
+     * @Route("/logout", name="app_logout", methods="POST")
      */
     public function logout()
     {
