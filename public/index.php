c25b44091117a19b58f1904561289b3a81dabe7d
timotheegrollier <timotheegrollier@gmail.com>
2021-07-22 01:13:44 +0200

    re

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <?php
 // for imageUploads
-    ini_set('memory_limit', '200M');
+// ini_set('memory_limit', '200M');
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
